7218d3f63ee428e68b3c3d4aa9c60a4bed85abf4
Furqan Khan <160186238+furqanalikhan1@users.noreply.github.com>
2024-03-07 01:23:59 +0500

    Add files via upload
    it is may first assinment

---
+++ ex1.ts
@@ -0,0 +1,556 @@
+//    tesk 3
+
+let person="furqan khan";
+console.log("Hello",person,"would you like to learn typescript?");
+
+//      tesk 4
+
+
+// let person="furqan khan";
+// console.log (person.toLowerCase());
+// console.log (person.toUpperCase());
+// console.log (person.replace(/\b\w/g,(ex1)=> ex1.toUpperCase()));
+
+//      tesk 5
+
+// let famousperson="furqan khan"
+// let message= "once said,A person who never made a misteke never"
+// console.log(famousperson,message);
+
+//    tesk 6
+
+// let whitespacename= "\n\tfurqan khan\t\n";
+//  console.log  (whitespacename);
+
+// let withoutwhitespacename=whitespacename.trim();
+// console.log(withoutwhitespacename);
+
+//  tesk 7/8
+// console.log(4+4);
+// console.log(12-4);
+// console.log(2*4);
+// console.log(12-4);
+// console.log(16/2);
+
+//   tesk  9
+// let favNumber=101;
+// let message="may favriet number is:";
+// console.log(message,favNumber);
+
+//    tesk  10
+//  cumment out
+
+//          tesk 11
+
+// let friendsName =["furqan","ali","khan","hamna",];
+// // console.log(friendsName);
+
+// friendsName.forEach(friendName => console.log(friendName));
+
+//      tesk 12
+
+// let friendsName =["furqan","ali","khan","hamna",];
+// friendsName.forEach(friendName => console.log(`Hellow ${friendName},how are you`));
+
+//        tesk 13
+
+// let transporttationModes =["car","bike","bus","trien"];
+// transporttationModes.forEach(mode=> console.log(`i would like to own a ${mode}`));
+
+//             tesk 14                                   
+
+// let guestList =["furqan","ali","khan","hamna",];
+// guestList.forEach(oneGuest => console.log(`salam ${oneGuest},would you like dinner with me?`));
+
+// //              tesk 15      
+// let guestList =["furqan","ali","khan","hamna"]
+// let dontCome=guestList[0];
+// console.log(dontCome,"nahi a sakta hai ");
+
+// guestList.splice(0,1,"maaz");
+// guestList.forEach(guest => console.log(`salam ${guest},you like dinner whit me`));
+
+
+//              tesk 16
+
+
+// // tesk 16
+// //    let guestArr  = ["ali khan","furqan","hamna"];
+// //  let canNotAttend="furqan"
+// //  let newguest= "hani"
+
+// // guestArr[guestArr.indexOf(canNotAttend)]=newguest;
+// // console.log(guestArr)
+
+// // guestArr .map((items)=>console.log(`Dear${items},i fond a bigger dinner tabel so i am invited more peopel.`)
+// // );
+
+// // // part 2
+// // let guestbeg  = "muezz"
+// // guestArr.unshift(guestbeg);
+// //  console.log(guestArr);
+
+// // probelem
+
+// // part 3
+// // let middleGuest= "fatima"
+// // let middleIndex= guestArr.length/3
+// // guestArr.splice(middleIndex,0,middleGuest)
+// // console.log(guestArr)
+
+// // part 4 append
+// // guestArr.push("nabeel")
+// // console.log(guestArr)
+
+// // part 5
+// // guestArr.map((itmes) =>
+//   // console.log(`dear ${itmes},youn are invited in the more people list dinner `)
+//   // );
+
+// // tesk 17
+// //  Inital of list of guests
+// // let guestArr  = ["ali","furqam"," khan","furqan","hamna"];
+// // console.log("due to limited space, only two people can be invitedfor dinner.");
+// //  while(guests.length > 2){
+//   // const removedGuest =guests.pop()
+//   // console.log(`sorry, ${removedGuest},you re still invited to dinner.`);}
+  
+//   //   probelem   
+// // let guestArr  = ["ali","furqan"];
+// // guestArr.forEach((guest) => console.log(`dear ${guest},youre stellinvited to dinner.`));
+
+  
+// // //  tesk 18
+
+// // let placesToVisit  =["tokay","pakistan","America","china","japan"];
+
+// // console.log("original order:",placesToVisit);
+// // console.log("Alphabetical order:",[...placesToVisit].sort());
+// // console.log("original order after sorting:",placesToVisit);
+// // console.log("Reverse alphabetical order:",[...placesToVisit].sort().Reverse);
+// // console.log("original order reverse sorting:",placesToVisit);
+
+// // placesToVisit.reverse();
+// // console.log("Back original order:",placesToVisit);
+
+// // placesToVisit.sort();
+// // console.log("sort in alphabetical order:",placesToVisit);
+// // placesToVisit.sort((a,b)=> b.localeCompare(a));
+// // console.log("sorted in reverse alphabetical order:",placesToVisit);
+
+// // let country=["pakistan","india","japan","china"];
+// // console.log ("list of country:");
+// // console.log(country);
+
+
+// // tesk 21
+// // let person={name:fname,age}={name:"furqan",fullname:"khan",gender:"male",age: 30  }
+// // console.log(person);
+
+
+
+// //      tesk 22
+// //  inteltional Error
+
+// // const days =["sunday","monday","tuesday","wednesday","thursday","fridar","satuerday"]
+// // // console.log(days{7});
+// // console.log(days[5])
+
+
+// //       tesk 23
+  
+// let car= "suberu";
+// console.log ("is car =='suberu'?predict ture")
+// console.log(car=='suberu')
+
+// console.log ("is car =='honda'?predict ture")
+// console.log(car =='honda')
+
+// console.log("is car=='suberu'?predict ture")
+// console.log(car =='suberu')
+
+
+// console.log("is car.length ==6? predict ture")
+// console.log(car.length == 6)  
+
+// console.log("is car=='SUBERU'?predict false")
+
+
+// console.log("is car.length !=10? predict false")
+// console.log(car.length != 10)  
+
+// console.log ("is 10> 15? predict false")
+// console.log(10 >25)
+
+
+// console.log ("is 3 <= 12? predict false")
+// console.log(3<=12)
+
+
+//  tesk  24
+
+// let name_1  ="furqan"
+// let name_2  ="ali"
+// let name_3  ="khan"
+
+// if(name_1 != name_3 ){
+// console.log("names are equal")
+// }
+
+// else{
+// console.log("names are not equal")
+// }
+
+
+// if(name_1 !=name_2){
+// console.log("names are equal")
+// }
+
+// let age_1:number= 18
+// let age_2:number= 22
+
+// if (age_1 ==18){
+//   console.log("eligibel for vote" )};
+  
+// if  (age_2==22){
+//     console.log  ("eligibel for vote in older" ) 
+// }
+
+// if (age_1 <= age_2){
+//     console.log("younger")
+// }
+// if (age_2 <= age_1){
+//     console.log("older")
+// }
+
+
+// if (age_1 ==18 && age_2 == 22){
+//     console.log("person is eligible for vote")
+// }
+// if (age_1 ==18||  age_2 != 22){
+// console.log("person is eligible not for vote")
+// }
+
+// let country:string []= ["pakistan","india","japan","chiana"]
+// // if (country.includes("pakistan")){
+//     // console.log("pakistan is in country list")
+// }
+
+// if (!country.includes("america")){
+// console.log("america is not includes in an array")
+// }
+
+
+//       tesk 25
+
+// let alien_color="green"
+// if (alien_color =="green")
+// console.log("you earn 5 points")
+
+
+//  let alien_color:string ="red"
+// if(alien_color=="red")
+// console.log("no output")
+
+//      tesk 26
+
+//  let alien_color="green"
+// if (alien_color =="green"){
+// console.log("player just earred 5 points for shooting the alien")
+// }else{
+//     console.log("player just earred 1 points for shooting")
+// }
+
+// let alien_color="red"
+// if (alien_color =="green"){
+// console.log("player just earred 5 points for shooting the alien")
+// }else{
+//     console.log("player just earred 10 points for shooting")
+// }
+
+//       tesk 27
+// let alien_color:string="yellow"
+
+// if (alien_color= "green"){
+// console.log("5 pionts")
+// } else if (alien_color== "yellow")
+// {
+//  console.log("10 pionts")
+// }else{
+// console.log("15 pionts")
+//     }
+
+//     tesk 28
+
+// let age:number=40
+// if(age<2){
+//     console.log("you are a baby")
+// }
+// else if (age <4){
+//     console.log("you are a toddler")
+// }
+// else if(age<13)
+// console.log("you are a kid")
+// else if (age <20){
+//     console.log("you are a teennager")
+// }
+//     else if (age <65){
+//         console.log("you are a adult")
+//     }
+//     else {
+//         console.log("you are a older")
+//     }
+
+
+//     tesk 29
+
+// let furit:string[]= ["banan","mango","apple"]
+
+// if(furit.includes("mango")){
+//     console.log("i love mango")
+// }
+
+// if(furit.includes("banan")){
+//     console.log("i hate banan")}
+    
+// if(furit.includes("apple")){
+//     console.log("i like apple")}
+
+
+//                      tesk 30
+
+// let users :string [] = ["admin","furqan","ali","hamza","khan"]
+
+// for(let user of users)
+//       if (user === "admin"){
+// console.log("hello admin ,would you like to see a status ")
+// } else {
+// console.log(`hello ${user},thak you for logging in again`)
+// }
+
+//                tesk  31\
+
+//  let users :string [] = ["admin","furqan","ali","hamza","khan"]
+
+// if (users.length === 0){
+//     console.log("we need to find some users")
+// } else{
+//     for(let user of users)
+//       if (user === "admin"){
+// console.log("hello admin ,would you like to see a status ")
+// } else {
+// console.log(`hello ${user},thak you for logging in again`)
+// }
+// // }
+// users =[]
+// if (users.length === 0){
+//     console.log("we need to find some users")
+// }
+
+//       tske   32
+
+
+//  let current_users :string [] = ["admin","furqan","Ali","Hamza","khan"]
+//  let new_usres:string[]=["adimn","hamna","Hanni","ali","tayyaba"]
+
+// let current_users_lower : string [] =current_users.map(user => user.toLowerCase())
+
+// for (let new_user of new_usres){
+//     if (current_users_lower.includes (new_user.toLowerCase())){
+//         console.log (`sorry ${new_user},that name is taken`)  
+
+//     }else{
+//         console.log(`yes  ${new_user},is still in avlible list`)
+//     }
+// }
+
+
+//          tesk 33 
+
+// let numbers : number [] = [1,2,3,4,5,6,7,8,9]
+
+// for (let number of numbers) {
+
+//     if (number ===1){
+//         console.log(`${number}st`)
+    
+//     }else if  (number ===2){
+//         console.log(`${number}nd`)
+
+// } else if (number ===3){
+//     console.log(`${number}rd`)
+
+// } else {
+// console.log (`${number}th`)
+// }
+// }
+
+
+//       tesk   34
+
+// let favourite_pizza :string [] =["pizz.one","pizzhot","chickin"]
+
+// for (let pizza of favourite_pizza){
+//     console.log(pizza)
+// }
+// console.log("\n")
+
+// for (let pizza of favourite_pizza){
+//     console.log (`I really like ${pizza} pizza!`)
+// }
+    
+// console.log(" I very love pizza!")
+
+
+//      tesk  35
+
+// let animals : string [] =["cat","dog","lion"]
+
+// for  (let animal of animals){
+//     console.log(animal)
+// }
+// console.log("\n")
+
+
+// for (let animal of animals){
+//     console.log(`A ${animal} has a tail`)
+// }
+// console.log ("\n  all of these are great pets ")
+
+
+//     tesk 36
+
+// function makeShirt (size : string, text : string) {
+//     console.log(` you order a ${size} shirt that says ${text} `)
+// }
+// makeShirt ('lerge',  "i love typesrcipt")
+// makeShirt ('medim',  "i need a big shirt")
+
+
+
+// //     tesk   37
+//     function makeShirt (size: string='large', text: string = ' I love typescript ') {
+// console.log(`you have order a ${size}, shrit that says ${text} `)
+//     }
+// // // makeShirt();
+// // makeShirt('mediem')
+
+// //  different message
+// makeShirt('small', 'I need a big shirt to wear')
+
+//    tesk  38
+
+// function describe_city (city: string, country: string = 'pakistan'): void{
+// console.log(`${city} is in ${country}`)
+// }
+
+
+// describe_city('karachi')
+// describe_city('france', 'europe')
+// describe_city('lahor','panjab')
+
+
+//      tesk  39
+
+// function cityCountry (city: string, Country: string ):string{
+//     return `${city}, ${Country}`
+// }
+// let c1 = cityCountry('lahor', 'pakistan')
+// let c2 = cityCountry('tokyo', 'japan')
+// let c3 = cityCountry('newyork', 'austeleya')
+
+
+// console.log(c1)
+// console.log(c2)
+// console.log(c3)
+
+
+//          tesk  40
+
+// function makeAlbum (artist: string, title: string): {artist: string, title: string}{
+//     const dictionaries = {
+//         artist: artist.charAt(0).toUpperCase() + artist.slice(1),
+//         title: title.charAt(0).toUpperCase() + title.slice(1)
+//     };
+//     return dictionaries;
+// }
+// let album =makeAlbum("ali","light")
+// console.log(album)
+
+// //  album =makeAlbum("bilal","red wave")
+// // console.log(album)
+// //  album =makeAlbum("ali","jaazrock")
+// // console.log(album)
+
+// //       tesk  41
+
+// function shwo_magicians(magicians: string[]):void{
+// for (const magician of magicians ){
+//     console.log(magician.charAt(0).toUpperCase()+magician.slice(1));
+// }
+// }
+// // const magician:string[]=["furqan","ali","khan"];
+// // shwo_magicians(magician)
+
+
+// // //     tesk   42
+// function make_guret(magicians:string[]):void{
+//     for(let i = 0; i < magicians.length; i++){
+//         magicians[i] = magicians[i] + '  the guret';
+// }
+// }
+// const magicians2: string[] =["furqan","ali","khan"];
+// make_guret(magicians2)
+// shwo_magicians(magicians2)
+
+
+// //      tesk 43
+
+
+// function make_guret2(magicians: string [] ): string[] {
+// const greatMagicians : string[] =[];
+//     for (let i = 0; i < magicians.length; i++){
+//         greatMagicians.push ( magicians[i] + '  the guret');
+// }
+//    return magicians;
+// }
+// const magicians3: string[] =["furqan","ali","khan"];
+
+
+// const greatmagicians2: string [] =  make_guret2 (magicians3);
+// shwo_magicians(greatmagicians2);
+// shwo_magicians(magicians3);
+
+
+//         tesk   44
+
+// function sandwich (...items : string []): void {
+//     console.log ("sandwich oder")
+
+//     for (let i = 0; i < items.length; i++){
+//         console.log(` - ${items[i]}`)
+
+//     }
+// }
+// console.log ("enjoy your sandwich furqan khan")
+
+// sandwich ('batter','chiken','beef')
+// sandwich('veg','cheese')
+// sandwich('garlic','veg batter')
+
+//      tesk   45
+
+// type car ={
+//     manufactuer :string
+//     model :(string)
+//     [key: string]: any;
+// }
+// function createcar(manufactuer: string, model :string, optional: Record<string, any>): car {
+//     return {
+//         manufactuer,
+//         model,
+//         ...optional
+//     }
+// }
+// const mycar: car = createcar("toyota","corolla",{color: "selver", year :"2024"})
+// console.log(mycar) 
